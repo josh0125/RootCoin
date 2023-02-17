43819b8dd4bb5dea5e3918b78e67d379ab8dda27
Joshua Allen <joshuatallen99@gmail.com>
2023-02-16 17:14:31 -0700

    Add project files.

---
+++ RootCoin/Block.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace RootCoin
+{
+    class Block
+    {
+        public int Index { get; set; }
+        public string PreviousHash { get; set; }
+        public string TimeStamp { get; set; }
+        public string Hash { get; set; }
+        public int Nonce { get; set; }
+        public List<Transaction> Tranactions { get; set; }
+
+        public Block(int index, string timestamp, List<Transaction> transactions, string previousHash = "")
+        {
+            this.Index = index;
+            this.TimeStamp = timestamp;
+            this.Tranactions = transactions;
+            this.PreviousHash = previousHash;
+            this.Hash = CalculateHash();
+            this.Nonce = 0;
+        }
+
+        public string CalculateHash()
+        {
+            string blockData = this.Index + this.PreviousHash + this.TimeStamp + this.Tranactions.ToString() + this.Nonce;
+            byte[] blockBytes = Encoding.ASCII.GetBytes(blockData);
+            byte[] hashByte = SHA256.Create().ComputeHash(blockBytes);
+
+            return BitConverter.ToString(hashByte).Replace("-", "");
+        }
+
+        public void Mine(int difficulty)
+        {
+            while (this.Hash.Substring(0, difficulty) != new String('0', difficulty))
+            {
+                this.Nonce++;
+                this.Hash = this.CalculateHash();
+                //Console.WriteLine("Hash: " + this.Hash);
+            }
+
+            Console.WriteLine("Block has been mined: " + this.Hash);
+        }
+    }
+}
---
+++ RootCoin/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace RootCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrivateKey key1 = new PrivateKey();
+            PublicKey wallet1 = key1.publicKey();
+
+            PrivateKey key2 = new PrivateKey();
+            PublicKey wallet2 = key2.publicKey();
+
+            Blockchain rootcoin = new Blockchain(2, 100);
+
+            Console.WriteLine("Start the Miner");
+            rootcoin.MinePendingTransactions(wallet1);
+            Console.WriteLine("\nBalance of Wallet1 is $" + rootcoin.GetBalanceofWallet(wallet1).ToString());
+
+            Transaction tx1 = new Transaction(wallet1, wallet2, 10);
+            tx1.SignTransaction(key1);
+            rootcoin.addPendingTransaction(tx1);
+            Console.WriteLine("Start the Miner");
+            rootcoin.MinePendingTransactions(wallet2);
+            Console.WriteLine("\nBalance of Wallet1 is $" + rootcoin.GetBalanceofWallet(wallet1).ToString());
+            Console.WriteLine("\nBalance of Wallet2 is $" + rootcoin.GetBalanceofWallet(wallet2).ToString());
+
+
+
+
+
+            string blockJSON = JsonConvert.SerializeObject(rootcoin, Formatting.Indented);
+            Console.WriteLine(blockJSON);
+
+            if (rootcoin.IsChainValid())
+            {  
+                Console.WriteLine("BlockChain is valid!"); 
+            }
+            else
+            {
+                Console.WriteLine("BlockChain is invalid!");
+
+            }
+        }
+    }
+}
